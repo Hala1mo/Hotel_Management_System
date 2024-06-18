7390ea237aa94bf1da7540f1d8e53650d79c115c
Hala1mo <halamontaser13@gmail.com>
2024-06-18 21:13:02 +0300

    update

---
+++ src/main/java/com/example/hotel_management_system/Services/ReservationService.java
@@ -0,0 +1,28 @@
+package com.example.hotel_management_system.Services;
+
+import com.example.hotel_management_system.DTO.Add_OnDTO;
+import com.example.hotel_management_system.DTO.ReservationDTO;
+import com.example.hotel_management_system.DTO.Room.RoomDTO;
+import com.example.hotel_management_system.DTO.Room.RoomDetailsInfoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ReservationService {
+    ResponseEntity<?> reserveBooking (ReservationDTO request);
+    List<ReservationDTO> retrieveReservationForSpecificCustomer(Long id, String firstName);
+    ResponseEntity<?> cancelAnReservation(long id);
+    ResponseEntity<?> approveReservationCancellation(long id);
+    // ResponseEntity<?> saveAdditionToSpecificBooking(List<Add_OnDTO> request, long id);
+    ResponseEntity<?> modifyBooking(long id,ReservationDTO request);
+    List<RoomDTO> retrieveRoomsForSpecficReservation(long id);
+
+    ResponseEntity<?> confirmReservation( Long id);
+    ResponseEntity<?> checkIn( Long id);
+    ResponseEntity<?>  checkOut(long id);
+
+    List<ReservationDTO>retrieveReservationsBySpecificDates(Date checkIn, Date checkOut);
+
+}
